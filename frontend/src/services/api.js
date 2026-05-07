086a7bbe1f174ca1701d74d5adfc39a14cd27faa
Nikunj Rana <8891nikunjrana@gmail.com>
2026-05-08 02:23:31 +0530

    update backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://portfolio-backend-crxb.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
